fdf39c6dfdaf2ca7e3ec0488c8766e9206736d89
Cokro Siamdani <cokrosiamdani@gmail.com>
2020-05-09 02:53:19 +0700

    com

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <Header></Header>
+        <ListStatus></ListStatus>
+    </div>
+</template>
+
+<script>
+import Header from './components/Header'
+import ListStatus from './components/ListStatus'
+
+export default {
+    name: 'App',
+    components: {
+        Header,
+        ListStatus
+    },
+    data() {
+        return {
+
+        };
+    },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container-header">
+        <h1 id="Kamvan-Board">KamVan Board</h1>
+        <button id="New-Task">New Task</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+
+}
+</script>
+
+<style>
+
+</style>
