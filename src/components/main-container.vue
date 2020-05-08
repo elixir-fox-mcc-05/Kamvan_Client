30c43d2e1684c3177c9bcaedb6fde4ba10a0a769
Ottoyd <ugaipribadi@gmail.com>
2020-05-08 20:53:13 +0700

    IntoTheCloud

---
+++ src/components/main-container.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="sub-container">
+    <div class="head-container-1">{{headerName}}
+      <button class="btn-add" @click="addTask">+</button>
+    </div>
+		<SubContainer @fect="fect" :items="items" :status="status"></SubContainer>
+  </div>
+</template>
+<script>
+import SubContainer from './sub-container'
+	export default {
+		name: 'MainContainer',
+    props: ['items','headerName','status'],
+    components : {
+    SubContainer,
+    },
+    methods: {
+      addTask(){
+        localStorage.task= this.$props.status;
+        this.$emit('fect')
+        this.$emit('addTask')
+        
+      },
+      fect() {
+        this.$emit('fect')
+      }
+    }
+	}
+</script>
+
+<style>
+  .btn-add {
+    width: 30px;
+    background-color: green;
+    font-weight: bold;
+    float: right;
+  }
+</style>
