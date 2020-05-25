d4fdd0213ad04c28146ed8a2cff20a5ca597aba6
amirfaisalz <amirsakti27@gmail.com>
2020-05-25 12:27:26 +0700

    change url server

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const server = axios.create({
-  baseURL: "http://localhost:3000"
+  baseURL: "https://stark-thicket-67122.herokuapp.com/"
 });
 
 export default server;
