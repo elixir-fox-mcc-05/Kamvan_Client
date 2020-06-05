c6bb8c3ee0cf1e7603046071dbf09fb7c305e1f7
Cokro Siamdani <cokrosiamdani@gmail.com>
2020-06-05 17:11:37 +0700

    deploy0

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const server = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://murmuring-stream-85707.herokuapp.com/",
 });
 
 export default server;
